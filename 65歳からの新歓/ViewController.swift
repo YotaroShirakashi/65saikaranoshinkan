528afecbe635fff796f06153a0666fef4dc437fe
白樫陽太郎 <you@shirakashiyoutarou-no-MacBook-Pro.local>
2016-08-02 16:05:46 +0900

    first commit

---
+++ 65歳からの新歓/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  65歳からの新歓
+//
+//  Created by 白樫陽太郎 on 2016/08/02.
+//  Copyright © 2016年 白樫陽太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
